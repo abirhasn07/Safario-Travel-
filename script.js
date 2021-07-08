1ef2f905f0e3cc4caf79abf91a69b3c0171d453a
abirhasn07 <iamabirhasan07@gmail.com>
2021-07-08 20:06:44 +0600

    travel-websites

---
+++ script.js
@@ -0,0 +1,18 @@
+const testhandler=()=>{
+  document.querySelector('.icon-group').classList.toggle('active')
+  document.querySelector('.nav-links').classList.toggle('active')
+  document.querySelector('.link').classList.toggle('active')
+}
+
+
+// const StickyNavbar=()=>{
+//   document.querySelector('.navigationBar').classList.toggle('sticky',scrollY>0)
+
+//   console.log("scroll");
+// }
+
+// window.addEventListener("scroll",function(){
+//   const sticky=document.querySelector('.navigationBar')
+//   sticky.classList.toggle('sticky',window.scrollY>0)
+// })
+
